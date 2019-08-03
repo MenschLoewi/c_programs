16249dc9532f840ea03aa655e3a978904e831964
Fabian Winnebald <winnebald92@gmail.com>
2019-08-03 12:13:41 +0200

    Created dual_und_hexadezimalsystem.c and added description

---
+++ dual_und_hexadezimalsystem.c
@@ -0,0 +1,11 @@
+//Darstellung des Dual- und Hexadezimalsystems in C, Rechenregeln und Tipps
+//Bitweise-Operatoren und nuetzliche Makros
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]){
+    puts("Hello, Dual");
+    return 0;
+}
