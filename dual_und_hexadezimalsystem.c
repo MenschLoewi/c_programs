68cd7e3046fad5e90c60ac2c06aee7e94cdd0c43
Fabian Winnebald <winnebald92@gmail.com>
2019-08-03 12:52:19 +0200

    Wrote assertions for function prototypes thus far

--- dual_und_hexadezimalsystem.c
+++ dual_und_hexadezimalsystem.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 
 char *decimal_to_binary(const char *decimal);
@@ -17,6 +18,19 @@ char *bin_mul(const char *bin1, const char *bin2);
 char *bin_div(const char *bin1, const char *bin2);
 
 int main(int argc, char *argv[]){
-    puts("Hello, Dual");
+    assert(strcmp("1010",decimal_to_binary("10"))==0);
+    assert(strcmp("10",binary_to_decimal("1010"))==0);
+
+    assert(strcmp("64",decimal_to_hexadecimal("100"))==0);
+    assert(strcmp("100",hexadecimal_to_decimal("64"))==0);
+
+    assert(strcmp("64",binary_to_hexadecimal("1100100"))==0);
+    assert(strcmp("1100100",hexadecimal_to_binary("64"))==0);
+
+
+    assert(strcmp("1010",bin_add("10","1000")==0);
+    assert(strcmp("10",bin_sub("1010","1000")==0);
+    assert(strcmp("1010",bin_mul("101","10")==0);
+    assert(strcmp("101",bin_div("1010","10")==0);
     return 0;
 }
