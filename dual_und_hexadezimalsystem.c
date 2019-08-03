68dc1d7af2bcb542070fbb1d085cb93d0f1f7344
Fabian Winnebald <winnebald92@gmail.com>
2019-08-03 12:34:04 +0200

    Added function prototypes for bin/hex/dec conversions and binary arithmetic

--- dual_und_hexadezimalsystem.c
+++ dual_und_hexadezimalsystem.c
@@ -1,9 +1,20 @@
-//Darstellung des Dual- und Hexadezimalsystems in C, Rechenregeln und Tipps
-//Bitweise-Operatoren und nuetzliche Makros
-
 #include <stdio.h>
 #include <stdlib.h>
 
+
+char *decimal_to_binary(const char *decimal);
+char *binary_to_decimal(const char *binary);
+
+char *decimal_to_hexadecimal(const char *decimal);
+char *hexadecimal_to_decimal(const char *hexadecimal);
+
+char *binary_to_hexadecimal(const char *binary);
+char *hexadecimal_to_binary(const char *hexadecimal);
+
+char *bin_add(const char *bin1, const char *bin2);
+char *bin_sub(const char *bin1, const char *bin2);
+char *bin_mul(const char *bin1, const char *bin2);
+char *bin_div(const char *bin1, const char *bin2);
 
 int main(int argc, char *argv[]){
     puts("Hello, Dual");
